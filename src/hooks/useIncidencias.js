307d8f24a625683def6c54bb72cb201ca332861a
Haza <hazael.fernandez@gmail.com>
2022-05-27 19:15:50 +0200

    atascado en el fecth (da error)

---
+++ src/hooks/useIncidencias.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { getAllIncidenciasService } from "../services";
+
+const useIncidencias = () => {
+  const [incidencias, setIncidencias] = useState([]); //primer valor del stado y segunda funcion para actualizar el estado
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const loadIncidencias = async () => {
+      try {
+        setLoading(true);
+
+        const data = await getAllIncidenciasService();
+
+        setIncidencias(data);
+      } catch (error) {
+          setError(error.message);
+      }finally{
+          setLoading(false)
+      }
+    };
+
+    loadIncidencias();
+
+  }, []);
+
+  return { incidencias, loading, error };
+};
+
+export default useIncidencias;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,17 @@
-export const Homepage =()=>{
-    return <section>
-        <h1>INCIDENCIAS</h1>
-        <p> aquí iran las incidencias de accesibilidad</p>
+import useIncidencias from "../hooks/useIncidencias";
+
+export const Homepage = () => {
+  const { incidencias, loading, error } = useIncidencias();
+
+  if (loading) return <p>cargando incidencias...</p>;
+  if (error) return <p>{error}</p>; 
+
+  console.log(incidencias);
+
+  return (
+    <section>
+      <h1>INCIDENCIAS</h1>
+      <p> aquí iran las incidencias de accesibilidad</p>
     </section>
-}
+  );
+};
